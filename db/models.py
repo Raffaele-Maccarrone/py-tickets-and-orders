8e6b96b5b8d770b12818f88e837798f9d6f1c3d2
Raffaele Maccarrone <raffaele.alves.maccarrone@gmail.com>
2026-06-01 16:56:11 -0300

    Solução

--- db/models.py
+++ db/models.py
@@ -81,7 +81,7 @@ class Order(models.Model):
             super().save(*args, **kwargs)
 
     def __str__(self) -> str:
-        return f"<Order: {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}>"
+        return f"{self.created_at.strftime('%Y-%m-%d %H:%M:%S')}>"
 
 
 class Ticket(models.Model):
@@ -119,8 +119,7 @@ class Ticket(models.Model):
     def __str__(self) -> str:
         movie = self.movie_session.movie.title
         time = self.movie_session.show_time
-        return (f"<Ticket:"
-                f" {movie}"
+        return (f" {movie}"
                 f" {time.strftime('%Y-%m-%d %H:%M:%S')}"
                 f" (row: {self.row},"
                 f" seat: {self.seat})>"
